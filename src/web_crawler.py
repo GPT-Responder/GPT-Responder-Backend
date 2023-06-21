b72cfa8a8dbefbbd67c5f4242f7a962baf681997
Matthew Stefanovic <matthew@stefanovic.us>
2023-06-21 17:04:16 -0400

    Added basic logging functionality

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -1,48 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
-import logging, colorlog
+import logging
+import colorlog
 
-# Create a logger
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
 
-# Create console handler
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# Create formatter and add it to the handlers
-formatter = colorlog.ColoredFormatter(
-    "%(log_color)s[%(levelname)s] [%(asctime)s] - %(message)s",
-    datefmt=None,
-    reset=True,
-    log_colors={
+def setup_logger(name, log_level=logging.DEBUG, stream_handler_level=logging.DEBUG):
+    # Define log colors
+    log_colors = {
         "DEBUG": "cyan",
         "INFO": "green",
         "WARNING": "yellow",
         "ERROR": "red",
         "CRITICAL": "red,bg_white",
-    },
-    secondary_log_colors={},
-    style="%",
-)
+    }
 
-ch.setFormatter(formatter)
+    # Create a logger
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
 
-# Add the handlers to the logger
-logger.addHandler(ch)
+    # Create console handler
+    ch = logging.StreamHandler()
+    ch.setLevel(stream_handler_level)
+
+    # Create formatter and add it to the handlers
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s[%(levelname)s] [%(asctime)s] - %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors=log_colors,
+        secondary_log_colors={},
+        style="%",
+    )
+
+    ch.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(ch)
+
+    return logger
 
 
 def get_page(url):
-    logging.info(f"Requesting page: {url}")
+    # TODO: Set up exception handeling
+    logger.info(f"Requesting page: {url}")
     page = requests.get(url)
     doc = BeautifulSoup(page.text, "html.parser")
-
     article = doc.article
 
     return article
 
 
+def start():
+    global logger
+
+    logger = setup_logger(__name__)
+
+
 if __name__ == "__main__":
+    start()
     page = get_page(
         "https://www.stetson.edu/administration/information-technology/help-desk/"
     )
