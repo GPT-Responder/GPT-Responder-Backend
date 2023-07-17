541e5656e718b13486ff73dea7d26ddd9283e8c2
Matthew Stefanovic <matthew@stefanovic.us>
2023-07-17 17:11:50 -0400

    Added weaviate docker-compose and weviate setup in python

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -1,12 +1,32 @@
 from typing import Optional
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from readability import Document
 import requests
 import logging
 import colorlog
+import weaviate
+import os
 
 
-def setup_logger(name, log_level=logging.DEBUG):
+def setup_weaviate_db() -> weaviate.Client:
+    weaviate_api_key: Optional[str] = os.getenv("WEAVIATE_API_KEY")
+
+    if weaviate_api_key is None:
+        raise ValueError("WEAVIATE_API_KEY environment variable is not set.")
+
+    auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
+
+    # Instantiate the client with the auth config
+    client = weaviate.Client(
+        url="https://localhost",
+        auth_client_secret=auth_config,
+    )
+
+    return client
+
+
+def setup_logger(name, log_level=logging.INFO) -> logging.Logger:
     """
     Sets up the logger
     """
@@ -77,19 +97,34 @@ def get_page(url: str) -> Optional[str]:
 
 def parse_html_for_vector_db(html: str) -> list[str]:
     """
-    Takes in HTML input and returns a list of strings spilit up to be used
-    in a vector database.
+    Takes in HTML input and returns an array of strings.
     """
     logger.info(f"Parsing HTML")
     soup = BeautifulSoup(html, "html.parser")
     paragraphs = soup.find_all("p")
 
     # Extract text from each <p> tag and add it to a list
-    p_list = [p.get_text() for p in paragraphs]
-    for pos, p in enumerate(p_list):
-        logger.debug(f"P tag {pos}: {p}")
+    paragraph_data = [p.get_text() for p in paragraphs]
+    for pos, p in enumerate(paragraph_data):
+        logger.debug(f"Added <p> tag {pos}: {p}")
 
-    return p_list
+    # TODO: Add something that parses tables
+    # Extract text from <table> and addes it to a list with some formating
+    # tables = soup.find_all("table")
+    #
+    # for table_pos, table in enumerate(tables):
+    #     rows = table.find_all("tr")
+    #     table_data = []
+    #     for row in rows:
+    #         cols = row.find_all(
+    #             "td"
+    #         )  # Change this to 'th' if you want to extract header cells
+    #         cols = [col.get_text() for col in cols]
+    #         table_data.append(cols)
+    #     for row_pos, row in enumerate(table_data):
+    #         logger.debug(f"Added row {row_pos} to table {table_pos}: {row}")
+
+    return paragraph_data
 
 
 def add_to_vector_db() -> None:
@@ -101,11 +136,17 @@ def add_to_vector_db() -> None:
 
 def start() -> None:
     global logger
+    load_dotenv()
 
-    logger = setup_logger(__name__)
-    page = get_page("https://www.stetson.edu/other/about/history.php")
+    weaviate_client: weaviate.Client = setup_weaviate_db()
+    logger: logging.Logger = setup_logger(__name__, logging.DEBUG)
+
+    page = get_page(
+        "https://catalog.stetson.edu/undergraduate/arts-sciences/computer-science/computer-science-bs/"
+    )
     page_info = parse_html_for_vector_db(page)
 
 
+# TODO: This will be removed at somepoint
 if __name__ == "__main__":
     start()
