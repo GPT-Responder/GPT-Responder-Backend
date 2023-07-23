a8d53a33eef7e5a2754d49e51492c812c7526b03
Matthew Stefanovic <matthew@stefanovic.us>
2023-07-22 23:27:26 -0400

    removed redundent schema

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -57,7 +57,7 @@ def setup_weaviate_db() -> None:
 
     logger.info("Checking if API Keys exist")
 
-    # TODO: Check if there is a better way to write this 
+    # TODO: Check if there is a better way to write this
     if weaviate_api_key is None:
         error_message: str = "WEAVIATE_API_KEY environment variable is not set."
         logger.error(error_message)
@@ -80,7 +80,7 @@ def setup_weaviate_db() -> None:
 
     logger.info("Connected to Weaviate database")
 
-    # TODO: figure out how to check if the class is already made 
+    # TODO: figure out how to check if the class is already made
     website = {
         "class": "Webpage",
         "description": "A webpage from a website specified in the whitelist",
@@ -101,16 +101,10 @@ def setup_weaviate_db() -> None:
                 "description": "The content of the webpage",
                 "dataType": ["text"],
             },
-            # TODO: This is probally redundent, Weaviate keeps track of this already
-            {
-                "name": "last_updated",
-                "description": "The date when this entry was last modified",
-                "dataType": ["date"],
-            },
         ],
     }
 
-    # weaviate_client.schema.create_class(website) 
+    # weaviate_client.schema.create_class(website)
 
 
 def get_page(url: str) -> Document:
@@ -123,11 +117,12 @@ def get_page(url: str) -> Document:
 
     except requests.HTTPError as http_err:
         logger.error(f"HTTP error occurred: {http_err}")
-        return
+        raise
 
     except Exception as err:
         logger.error(f"Other error occurred: {err}")
-        return
+        raise
+
     else:
         logger.info(f"Site {url} receved, parsing text")
 
@@ -135,7 +130,7 @@ def get_page(url: str) -> Document:
 
     if doc:
         logger.info(f"Found text on {url}")
-        
+
         logger.debug(f"HTML from {url}:\n{doc.summary()}")
     else:
         logger.info(f"No text found on {url}")
@@ -151,7 +146,7 @@ def parse_html_for_vector_db(html: str) -> list[str]:
     paragraphs = soup.find_all("p")
 
     # Extract text from each <p> tag and add it to a list
-    # TODO: Make this more readable 
+    # TODO: Make this more readable
     paragraph_data = [p.get_text() for p in paragraphs if p.get_text().strip()]
     for pos, p in enumerate(paragraph_data):
         logger.debug(f"Added <p> tag {pos}: {p}")
@@ -160,9 +155,10 @@ def parse_html_for_vector_db(html: str) -> list[str]:
 
     return paragraph_data
 
+
 def add_webpage_to_db(site: str) -> bool:
     # Getting info from webpage
-    logger.info(f'Parsing HTML for {site}')
+    logger.info(f"Parsing HTML for {site}")
     webpage: Document = get_page(site)
     current_time: datetime = datetime.now(timezone.utc)
     title: str = webpage.title()
@@ -170,62 +166,54 @@ def add_webpage_to_db(site: str) -> bool:
     data: list[dict[str, str]] = []
 
     # Putting webpage info in json format
-    logger.info(f'Formating content for {site}')
+    logger.info(f"Formating content for {site}")
     for content in page_contents:
         info: dict[str, str] = {
-                    'title': title,
-                    'url': site, 
-                    # 'section': section # TODO: Code in section
-                    'content': content,
-                    'last_updated': current_time.isoformat()
-                }
+            "title": title,
+            "url": site,
+            "content": content,
+        }
         data.append(info)
 
     # Batch adding data to Weaviate Database
-    logger.info(f'Adding content to Weaviate database for {site}')
+    logger.info(f"Adding content to Weaviate database for {site}")
     with weaviate_client.batch as batch:
         batch.batch_size = 100
 
         for i, d in enumerate(data):
-            logger.debug(f'Adding the following to Weaviate Database:\n{d}')
+            logger.debug(f"Adding the following to Weaviate Database:\n{d}")
             properties = {
-                    'title': d['title'],
-                    'url': d['url'], 
-                    # 'section': section # TODO: Code in section
-                    'content': d['content'],
-                    'last_updated': d['last_updated']
-                    }
+                "title": d["title"],
+                "url": d["url"],
+                "content": d["content"],
+            }
             weaviate_client.batch.add_data_object(properties, "Webpage")
-    logger.debug(f'Content added to Weaviate database for {site}')
+    logger.debug(f"Content added to Weaviate database for {site}")
 
-    return True # TODO: make this return false if getting the webpage fails
+    return True  # TODO: make this return false if getting the webpage fails
 
 
 def start() -> None:
-    try: 
+    setup_logger(__name__, logging.DEBUG)
+
+    try:
         load_dotenv()
 
-
-        setup_logger(__name__, logging.DEBUG)
         setup_weaviate_db()
         webpages = [
-                    "https://catalog.stetson.edu/undergraduate/arts-sciences/computer-science/computer-science-bs/",
-                    "https://www.stetson.edu/other/academics/undergraduate/education.php"
-                    "https://www.stetson.edu/law/academics/clinical-education/federal-litigation-internship.php"
-                ] 
+            "https://catalog.stetson.edu/undergraduate/arts-sciences/computer-science/computer-science-bs/",
+            "https://www.stetson.edu/other/academics/undergraduate/education.php",
+            "https://www.stetson.edu/law/academics/clinical-education/federal-litigation-internship.php",
+        ]
 
         for site in webpages:
             add_webpage_to_db(site)
 
         import json
-        print(
-                json.dumps(
-                    weaviate_client.data_object.get(),
-                    indent = 2
-                    )
-                )
+
+        print(json.dumps(weaviate_client.data_object.get(), indent=2))
     except KeyboardInterrupt:
-        logger.WARNING('Exiting program, have a nice day :)')
+        logger.WARNING("Exiting program, have a nice day :)")
 
 
 # TODO: This will be removed at somepoint
