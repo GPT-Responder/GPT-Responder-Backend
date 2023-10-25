7ecb0cc7cdad483b3f1acabcdaf5ff446f50c5f4
Matthew Stefanovic <ms@stefanovic.us>
2023-10-25 13:44:44 -0400

    Added midleware to API

--- src/api.py
+++ src/api.py
@@ -1,11 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from weaviate_handler import WeaviateHandler
 from chat_gpt import ChatGPT
 from logger_setup import setup_logger
 
 logger = setup_logger()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -25,12 +34,19 @@ async def question(question: str):
     role = "You are an admissions officer at Stetson univerisity. Using only the context provided, you will answer emailed questions. Do not add an email signature. Make sure to always include the webpage link."
     context = response["data"]["Get"]["Webpage"][0]["content"]
     url = response["data"]["Get"]["Webpage"][0]["url"]
-    title = response["data"]["Get"]["Webpage"][0]["title"]
 
     chatgpt = ChatGPT()
 
-    content = f"Question: {question}\nContext: {context} URL: {url}"
-    gpt_response = chatgpt.prompt(content, role=role)["choices"][0]["message"]["content"]
+    prompt = f"Question: {question}\nContext: {context} URL: {url}"
+    token_count = chatgpt.string_to_tokens(prompt)
+    
+    if token_count > 4096:
+        logger.warning(f"Prompt is too long ({token_count} tokens), using 16k model instead")
+        gpt_response = chatgpt.prompt(prompt, role, model="gpt-3.5-turbo-16k")
+    else:
+        gpt_response = chatgpt.prompt(prompt, role)
+
+    gpt_response = gpt_response["choices"][0]["message"]["content"]
     
     return { "response": gpt_response }
 
